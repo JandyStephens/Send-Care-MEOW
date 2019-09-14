c6777784ebc7e59700ca616088e10dd59fefd758
Jonathan C <jsea@uw.edu>
2019-09-14 13:16:14 -0700

    Setup state handling + some separating of concerns

---
+++ assets/js/settings.js
@@ -0,0 +1,3 @@
+// Ajax Query Templates
+const twitterQueryURL = (whatever, it, needs) => `http://whatever/?here=${whatever}&we=${it}&go=${needs}`;
+const postmatesQueryURL = (whatever, it, needs) => `http://whatever/?here=${whatever}&we=${it}&go=${needs}`;
