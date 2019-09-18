ea92d50e2ea29c1c34e3edc3fdf409750c9b3297
Jandy <reverdy@protonmail.com>
2019-09-18 12:20:06 -0700

    message displaying in preview screen

--- assets/js/states/preview-screen.js
+++ assets/js/states/preview-screen.js
@@ -7,28 +7,28 @@ var previewScreenState = {
       ui.get$FromRef("preview-screen").css("position", "static");
     });
 
-        // Detach button click events here too (this handles the case of using forward/back navigation buttons causing multiple fade ins/outs)
-        previewScreenState.clearButtonClickHandlers();
+    // Detach button click events here too (this handles the case of using forward/back navigation buttons causing multiple fade ins/outs)
+    previewScreenState.clearButtonClickHandlers();
 
-        // START: General code to run after this screen finishes transitioning out and immediately before the state switches
+    // START: General code to run after this screen finishes transitioning out and immediately before the state switches
 
-        // Destroy the spotify player
-        ui.get$FromRef('spotify-player-container').empty();
+    // Destroy the spotify player
+    ui.get$FromRef("spotify-player-container").empty();
 
-        // END: General code to run after this screen finishes transitioning out and immediately before the state switches
-    },
-    
-    loadState: function (prevState) {
+    // END: General code to run after this screen finishes transitioning out and immediately before the state switches
+  },
 
-        // START: Code to run before this screen starts transitioning in
-        // I'd suggest putting any changes here you want to be visible on the screen when it transitions in.
+  loadState: function(prevState) {
+    // START: Code to run before this screen starts transitioning in
+    // I'd suggest putting any changes here you want to be visible on the screen when it transitions in.
 
-        // If we have a spotify playlist set
-        if (database.exists('spotify-playlist')) {
-
-            var playlistID = database.loadObject('spotify-playlist').playlistID;
-            ui.get$FromRef('spotify-player-container').html(`<iframe class="spotify-player" src="https://open.spotify.com/embed/playlist/${playlistID}" width="600" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`);
-        }
+    // If we have a spotify playlist set
+    if (database.exists("spotify-playlist")) {
+      var playlistID = database.loadObject("spotify-playlist").playlistID;
+      ui.get$FromRef("spotify-player-container").html(
+        `<iframe class="spotify-player" src="https://open.spotify.com/embed/playlist/${playlistID}" width="600" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
+      );
+    }
 
     // END: Code to run before this screen starts transitioning in
 
@@ -40,6 +40,13 @@ var previewScreenState = {
       // I'd suggest putting any changes here you want to activate once the screen is done transitioning in.
 
       //   >>> Replace this line with any code that may make sense here <<<
+      $(".showMsg").empty();
+      var pullMsg = localStorage.getItem("user-message");
+      var msgArea = $("<p>");
+      msgArea.text(pullMsg);
+      // msgArea.attr("msgDisplay", pullMsg);
+      $(".showMsg").append(msgArea);
+
       $(".showGifs").empty();
       for (i = 0; i < 3; i++) {
         var gifUrl = localStorage.getItem("gif" + i);
@@ -61,12 +68,12 @@ var previewScreenState = {
 
         // END: Code to run immediately upon clicking the use button
 
-                // Detach the click event handlers from the main screen buttons (so they're not clicked more than once)
-                previewScreenState.clearButtonClickHandlers();
+        // Detach the click event handlers from the main screen buttons (so they're not clicked more than once)
+        previewScreenState.clearButtonClickHandlers();
 
-                // Switch to the package sent screen
-                window.location.hash = 'package-sent-screen';
-            });
+        // Switch to the package sent screen
+        window.location.hash = "package-sent-screen";
+      });
 
       // Attach a click event handler to the cancel button (done here so its not clickable until fully on screen)
       ui.get$FromRef("cancel-preview-button").on("click", function() {
@@ -78,29 +85,28 @@ var previewScreenState = {
 
         // END: Code to run immediately upon clicking the cancel button
 
-                // Detach the click event handlers from the main screen buttons (so they're not clicked more than once)
-                previewScreenState.clearButtonClickHandlers();
+        // Detach the click event handlers from the main screen buttons (so they're not clicked more than once)
+        previewScreenState.clearButtonClickHandlers();
 
-                // Switch back to the create package screen
-                window.location.hash = 'create-package-screen';
-            });
-        });
-    },
+        // Switch back to the create package screen
+        window.location.hash = "create-package-screen";
+      });
+    });
+  },
 
-    clearButtonClickHandlers: function () {
-
-        ui.get$FromRef('use-preview-button').off('click');
-        ui.get$FromRef('cancel-preview-button').off('click');
-    },
+  clearButtonClickHandlers: function() {
+    ui.get$FromRef("use-preview-button").off("click");
+    ui.get$FromRef("cancel-preview-button").off("click");
+  }
 };
 
 // Add references to jQuery selections of HTML elements that are permanently on the page
-ui.add$ToRef('preview-screen', '#preview-screen');
+ui.add$ToRef("preview-screen", "#preview-screen");
 
-ui.add$ToRef('spotify-player-container', '.spotify-player-container');
+ui.add$ToRef("spotify-player-container", ".spotify-player-container");
 
-ui.add$ToRef('cancel-preview-button', '.cancel-preview-button');
-ui.add$ToRef('use-preview-button', '.use-preview-button');
+ui.add$ToRef("cancel-preview-button", ".cancel-preview-button");
+ui.add$ToRef("use-preview-button", ".use-preview-button");
 
 // Add our new state to the state machine
 sm.addState("preview-screen", previewScreenState);
